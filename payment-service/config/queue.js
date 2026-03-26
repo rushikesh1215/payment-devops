a4b9dfb777db3f16198a55d62ee4f8adffa4d7c9
Rushikesh Jaybhaye <jaybhayerushikesh655@gmail.com>
2026-03-26 18:30:29 +0530

    added docker files

--- payment-service/config/queue.js
+++ payment-service/config/queue.js
@@ -1,11 +1,6 @@
-// FIX: force ioredis to use IPv4 (not ::1)
-
 import { Queue } from "bullmq";
 import IORedis from "ioredis";
 
-const connection = new IORedis({
-  host: "127.0.0.1",
-  port: 6379
-});
+const connection = new IORedis(process.env.REDIS_URL);
 
 export const ledgerQueue = new Queue("ledger-queue", { connection });
